23180f031d2f3e97f77e4a90fbb12608275d80eb
nousheen05 <21211a05k3@bvrit.ac.in>
2023-07-12 22:20:29 +0530

    learnt

---
+++ 1-fundamentals/14-helper-methods/app.js
@@ -0,0 +1,12 @@
+
+// document.write() --> It writes on the web page 
+// alert() --> its just a pop up message
+// console.log() --> To write or print onto to the console
+
+// Tip: console.log() is very helpful when you gonna print objects compared to other methods
+
+
+document.write("Heyyy guys");
+alert("Heelooo");
+console.log("Printing onto the console");
+console.log("Heyy");
---
+++ 1-fundamentals/15-variables/app.js
@@ -0,0 +1,39 @@
+// variables - store, access, modify the values
+// declare, assign the values
+// assigning can be done later too, modifying
+
+
+let name = "timmy";
+console.log(name);
+
+
+let fruit;
+fruit = "mango";
+console.log(fruit);
+fruit = "banana";
+console.log(fruit);
+
+
+let phone = "iphone";
+let version = 11;
+console.log(phone, version);
+
+
+
+//``````````variable naming rules````````````
+
+// can contain digits, letters, $, _
+// must start with letter or _ or $
+
+// no keyword
+// dont start with number
+
+
+// variable names are case sensitve
+// camelCase or underscore
+
+
+//````````````````` let vs const vs var`````````````
+
+// var - same as let but used in before ES6
+// const - cant reassign it
