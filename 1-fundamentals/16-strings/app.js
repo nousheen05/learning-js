72106eaa8fa9043d6655db886353dc87f14a75a3
nousheen05 <21211a05k3@bvrit.ac.in>
2023-07-13 07:05:56 +0530

    day 3

---
+++ 1-fundamentals/16-strings/app.js
@@ -0,0 +1,15 @@
+// Strings - we can use "" or ''
+const x = "Nousheen's smile is best";
+const y = 'Nousheen\'s auara is the best';
+
+console.log(x);
+console.log(y);
+
+
+// String concatenation
+
+let name = "youtube";
+let website = "https://"+ name + ".com";
+console.log(website);
+console.log("https://" + name + ".com");
+
---
+++ 1-fundamentals/17-numbers/app.js
@@ -0,0 +1,29 @@
+// JS - dynamically typed lnguage, no need of declaring datatypes
+// number - decimals/whole numbers
+
+// let num1 = 12;
+// console.log(num1);
+
+// let num2  = 12.3;
+// console.log(num2);
+// num2 = "12.3";
+// console.log(num2);
+
+
+
+
+//basic math operations
+
+let a  = 5;
+let b = 3;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+let no = a/b;
+console.log(no.toFixed(2));  //It always gives decimal value
+console.log(a%b);
+
+
+// operators
+// += -= *= /= ++ -- 
