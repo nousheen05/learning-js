67d7314ea2b52dea7e232ac0e8e731cfd8708fc8
nousheen05 <21211a05k3@bvrit.ac.in>
2023-07-13 20:30:58 +0530

    done with fundamentals

--- 1-fundamentals/17-numbers/app.js
+++ 1-fundamentals/17-numbers/app.js
@@ -27,3 +27,10 @@ console.log(a%b);
 
 // operators
 // += -= *= /= ++ -- 
+
+
+//Implicit type conversion
+
+console.log(2 + "45"); // number get converted to string
+console.log(2 - "45"); // string get converted to number
+console.log("2" - "1"); //strings get converted to numbers
---
+++ 1-fundamentals/18-dataTypes/app.js
@@ -0,0 +1,23 @@
+// data types - 7
+// string, number, null, undefined, boolean, symbol
+// objects - Arrays, objects, functions
+
+//string
+const name = "Tim";
+console.log(typeof name);
+
+//number
+const num = 22.3;
+console.log(typeof num);
+
+//boolean
+let val = true ;
+console.log(typeof val);
+
+// null
+const v = null;
+console.log(typeof v); //error from beginning in js // the output is object but it should be null
+
+//undefined
+let n;
+console.log(typeof n);
